25f55af2a8044cfbdc3f44a6b18661949ba78cdc
batiazinga <luc.nguyenthe@gmail.com>
2019-04-21 17:41:46 +0200

    Remove vim and simplify go environment

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 Some basic commands to set-up my workspace and start working.
-Do not run install.bash and install_vim.bash as sudo.
+
+# Install basic tools
+
+```bash
+./install.bash
+```
+
+# Install go
+
+See [official website](https://golang.org/).
+
+Copy/paste this to `~/.profile` to setup go environment
+```bash
+# set PATH so it includes go bin if go is installed
+if [ -d "/usr/local/go/bin" ] ; then
+    # standard go binaries
+    PATH="/usr/local/go/bin:$PATH"
+    # my go binaries
+    if [ -d "$(go env GOPATH)/bin" ] ; then
+        PATH="$(go env GOPATH)/bin:$PATH"
+    fi
+fi
+```
--- install.bash
+++ install.bash
@@ -3,8 +3,8 @@
 # do not run this as sudo
 
 echo ">> installing main tools"
-sudo apt-get update
-sudo apt-get install -y \
+sudo apt update
+sudo apt install -y \
     git-all \
     meld \
     build-essential \
@@ -13,18 +13,9 @@ sudo apt-get install -y \
 
 echo ""
 echo ">> copying .gitconfig file to home"
-cp $(pwd)/my_gitconfig ~/.gitconfig
+cp $(pwd)/.gitconfig ~/.gitconfig
 
 echo ""
-echo ">> create my go folders in Projects"
-if [ ! -d ~/Projects/dev-tools ]; then
-	mkdir -p ~/Projects/dev-tools/{bin,pkg,src}
-fi
-if [ ! -d ~/Projects/go-libs ]; then
-	mkdir -p ~/Projects/go-libs/{bin,pkg,src}
-fi
-
-echo ""
-echo ">> copying .bashrc_extension file to home"
-cp $(pwd)/my_bashrc_extension ~/.bashrc_extension
+echo ">> copying .bash_aliases file to home"
+cp $(pwd)/.bash_aliases ~/.bash_aliases
 
