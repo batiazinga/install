ce774cc5a27b63ba37e0935253c02b8c7acf4794
batiazinga <luc.nguyenthe@gmail.com>
2019-11-25 22:10:15 +0100

    Move to vscode

--- install.bash
+++ install.bash
@@ -18,8 +18,3 @@ cp $(pwd)/.gitconfig ~/.gitconfig
 echo ""
 echo ">> copying .bash_aliases file to home"
 cp $(pwd)/.bash_aliases ~/.bash_aliases
-
-echo ""
-echo ">> copying go-dev-tools.bash script to ~/bin"
-mkdir -p $HOME/bin
-cp $(pwd)/go-dev-tools.bash $HOME/bin/
